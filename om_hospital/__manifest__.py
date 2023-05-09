fa4532674dd799f9d4073bedaabd19111e7caacc
mohamed fares <faresabidi21@gmail.com>
2023-05-09 13:47:50 +0100

    first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    'name': "Hospital Management",
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': "Med Fares",
+    'summary': 'Hospital management system',
+    'description': """ Hospital management system """,
+    'depends': [],
+    'data': [
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+
+    'auto-install': False,
+    'license': 'LGPL-3',
+    'application': True,
+}
