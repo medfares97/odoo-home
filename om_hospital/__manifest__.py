559d01aa36234078519bb76bfd76b464ef439675
mohamed fares <faresabidi21@gmail.com>
2023-05-16 12:07:14 +0100

    all reps are deleted

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -6,11 +6,14 @@
     'author': "Med Fares",
     'summary': 'Hospital management system',
     'description': """ Hospital management system """,
-    'depends': [],
+    'depends': ['mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/male_patient_view.xml',
+        'views/appointment_view.xml'
     ],
     'demo': [],
 
