637630eb3671847e81ef897b8ccd389c904e9db9
mohamed fares <faresabidi21@gmail.com>
2023-05-17 16:22:21 +0100

    avancement HTA

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -4,9 +4,34 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'patient_id'
 
     #Relational fields
     #Add Many2one field
-    patient_id = fields.Many2one(comodel_name= 'hospital.patient', string="Patient")
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+    gender = fields.Selection(related='patient_id.gender', readonly=False)
     appointment_time = fields.Datetime(string='Appointment Time')
     booking_date = fields.Date(string='Booking Date')
+    ref = fields.Char(string='Reference')
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], default='draft', string="Status", required=True, tracking=True)
+
+
+    #how to define an onchange function
+    @api.onchange('patient_id') #specify the onchange
+    def onchange_patient_id(self): #field name
+        self.ref = self.patient_id.ref #get it from the patient model self.patient_id_ref
+
+    # function named 'action_test' for the Test Button
+
+    def action_test(self):
+        print("Button clicked !!!!!!")
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,3 +1,7 @@
+from datetime import date
+
+import dateutil.utils
+
 from odoo import  api, fields, models
 
 class HospitalPatient(models.Model):
@@ -7,7 +11,19 @@ class HospitalPatient(models.Model):
 
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date Of Birth')
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
     active = fields.Boolean(string="Active", default=True)
+    appointment_id = fields.Many2one(comodel_name='hospital.appointment', string="Appointments")
+
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
