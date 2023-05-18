4fc0ef4a7afd1397c69f54b33c3b930fb7196356
mohamed fares <faresabidi21@gmail.com>
2023-05-18 15:51:12 +0100

    avancement HTA3

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -1,18 +1,16 @@
-from odoo import  api, fields, models
+from odoo import api, fields, models
+
 
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
     _rec_name = 'patient_id'
-
-    #Relational fields
-    #Add Many2one field
     patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
     gender = fields.Selection(related='patient_id.gender', readonly=False)
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_date = fields.Date(string='Booking Date')
-    ref = fields.Char(string='Reference')
+    appointment_time = fields.Datetime(string='Appointment Time', help="Date of appointment")
+    booking_date = fields.Date(string='Booking Date', help="Date of booking")
+    ref = fields.Char(string='Reference', help="Reference from patient records")
     prescription = fields.Html(string='Prescription')
     priority = fields.Selection([
         ('0', 'Normal'),
@@ -24,14 +22,33 @@ class HospitalAppointment(models.Model):
         ('in_consultation', 'In Consultation'),
         ('done', 'Done'),
         ('cancel', 'Cancelled')], default='draft', string="Status", required=True, tracking=True)
+    doctor_id = fields.Many2one('res.users', string='Doctor')
 
-
-    #how to define an onchange function
-    @api.onchange('patient_id') #specify the onchange
-    def onchange_patient_id(self): #field name
-        self.ref = self.patient_id.ref #get it from the patient model self.patient_id_ref
+    # how to define an onchange function
+    @api.onchange('patient_id')  # specify the onchange
+    def onchange_patient_id(self):  # field name
+        self.ref = self.patient_id.ref  # get it from the patient model self.patient_id_ref
 
     # function named 'action_test' for the Test Button
 
     def action_test(self):
         print("Button clicked !!!!!!")
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Click Successfull',
+                'type': 'rainbow_man',
+            }
+        }
+
+    def action_in_consultation(self):
+        for rec in self:
+            rec.state = 'in_consultation'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
